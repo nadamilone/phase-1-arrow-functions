cacf7c6017e20ba717b4c2ee58f5d10c7c20861f
NADA MILONE <nadamilone@gmail.com>
2022-04-28 11:24:25 -0400

    commit tree 58f85e2525aeedd91dbfe86afa522dfbfdb8a296

---
+++ index.js
@@ -0,0 +1,10 @@
+// has a function expression called divide
+// divide divides 2000 by 100
+//has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+//has an arrow function called add
+//add arrow function takes two parameters and adds them together
+
+let divide = () => 2000/100
+let square = x => x * x;
+let add = (a, b) => a + b;
